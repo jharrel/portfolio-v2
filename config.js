cb4cab7bafa376509074f6c6ede62c1f1dd51935
jharrel <harrel.jason@gmail.com>
2020-02-06 13:41:40 -0800

    first commit

---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteTitle: 'JHarrel Design', // <title>
+  manifestName: 'Spectral',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/favicon.png',
+  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Jason Harrel',
+  subHeading: 'Front-End developer & Creative Service professional  ',
+  // social
+  socialLinks: [
+    {
+      style: 'brands',
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/jharrel',
+    },
+    // {
+    //   style: 'brands',
+    //   icon: 'fa-twitter',
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/onlyanubhav',
+    // },
+    {
+      style: 'solid',
+      icon: 'fa-envelope',
+      name: 'Email',
+      url: 'jharreldesign@gmail.com',
+    },
+  ],
+};
